3240cbbb28f6e48b1181c61a55d623ca264b01dd
Mengting Cai <caimengting777@gmail.com>
2024-08-02 19:47:31 +1000

    Normalize all the line endings

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue'
+// import JSONSolution from './components/JSONSolution.vue'
+</script>
+
+<template>
+  <JSON />
+  <!-- <JSONSolution />-->
+</template>
