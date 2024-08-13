6c5a792eb267842589bf4407ff2ee93a8864bf16
Mengting Cai <caimengting777@gmail.com>
2024-08-13 16:32:11 +1000

    week3-2

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="container-fluid mt-5">
+  <div class="container mt-5">
     <div class="row">
-      <div class="col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
+      <div class="col-md-8 offset-md-2">
         <h1 class="text-center">User Information Form</h1>
         <form @submit.prevent="submitForm">
           <div class="row mb-3">
-            <!-- <div class="col-md-6"> -->
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" v-model="formData.username" />
-            <!-- </div> -->
-            <!-- <div class="col-md-6"> -->
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" v-model="formData.password" />
-            <!-- </div> -->
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" v-model="formData.username" />
+            </div>
+            <div class="col-md-6">
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                v-model="formData.password"
+              />
+            </div>
           </div>
           <div class="row mb-3">
-            <!-- <div class="col-md-6"> -->
-            <div class="form-check">
-              <input
-                type="checkbox"
-                class="form-check-input"
-                id="isAustralian"
-                v-model="formData.isAustralian"
-              />
-              <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+            <div class="col-md-6">
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="isAustralian"
+                  v-model="formData.isAustralian"
+                />
+                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+              </div>
             </div>
-            <!-- </div> -->
-            <!-- <div class="col-md-6"> -->
-            <label for="gender" class="form-label">Gender</label>
-            <select class="form-select" id="gender" v-model="formData.gender">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-            <!-- </div> -->
+            <div class="col-md-6">
+              <label for="gender" class="form-label">Gender</label>
+              <select class="form-select" id="gender" v-model="formData.gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label
@@ -111,15 +116,5 @@ const submitForm = () => {
 }
 .list-group-item {
   padding: 10px;
-}
-
-.custom-element {
-  font-size: 14px; /* Default style for extra small devices */
-}
-
-@media (min-width: 576px) {
-  .custom-element {
-    font-size: 16px; /* Adjust for small devices and up */
-  }
 }
 </style>
