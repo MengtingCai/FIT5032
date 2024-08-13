d2efc1d906830e01a8c3a2425a90dbf6df5d5b9f
Mengting Cai <caimengting777@gmail.com>
2024-08-13 15:57:11 +1000

    week3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
-// import JSON from './components/JSON.vue'
-// import JSONSolution from './components/JSONSolution.vue'
-import Form from './components/LoginForm.vue'
+// import JSON from "./components/JSON.vue"
+// import JSONSolution from "./components/JSONSolution"
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <!-- <JSON /> -->
-  <!-- <JSONSolution />-->
+  <!-- <JsonSolution /> -->
   <Form />
 </template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-// import './assets/base.css'
-// import './assets/main.css'
+// import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import './style.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
