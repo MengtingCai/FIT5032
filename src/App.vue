fd599508574a8fc3967e6b952c42ade0f34e8f64
Mengting Cai <caimengting777@gmail.com>
2024-10-17 14:18:49 +1100

    Deploy site to gh-pages

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+// import JSON from "./components/JSON.vue"
+// import JSONSolution from "./components/JSONSolution"
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <!-- <JSON /> -->
+  <!-- <JsonSolution /> -->
+  <Form />
+</template>
+
+<style scoped></style>
